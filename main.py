ea78c83a7b5a8eebf5f0b8bd7f145f076eb289ec
jaeboklee <taland7@naver.com>
2024-06-01 23:41:53 +0900

    cors, delete added

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from todo import todo_router
 import uvicorn
 
 app = FastAPI()
 
+origins = ["http://127.0.0.5500", "http://35.127.37.0"]
+
+app.add_middleware(CORSMiddleware, allow_origins = origins, allow_credentials = True , allow_methods = ["*"], allow_headers = ["*"]
+
+)
 @app.get("/") # 
 async def welcome() -> dict:
     return {
--- todo.py
+++ todo.py
@@ -1,14 +1,22 @@
 from fastapi import APIRouter,Path
+from fastapi.middleware.cors import CORSMiddleware
 from model import Todo
+import uvicorn
 
 todo_router = APIRouter()
+
+
+origins = []
+
+
 
 todo_list = []
 todo_counter = 0
 
 @todo_router.post("/todo")
 async def add_todo(todo : Todo) -> dict:
-    
+    global todo_counter
+    todo.id = todo_counter = todo_counter + 1  #todo.id = len(todo_list) + 1
     todo_list.append(todo)
     return { "msg" : "todo added successfully"}
 
@@ -24,3 +32,10 @@ async def get_single_todo(todo_id : int = Path(..., title = "the id of the todo 
             return {"todo" : todo}
     return{"msg : there is no task with the id"}
 
+@todo_router.delete("/todo/{todo_id}")
+async def delete_todo(todo_id: int = Path(..., title="the ID of the todo to delete")) -> dict:
+    for index, todo in enumerate(todo_list):
+        if todo.id == todo_id:
+            del todo_list[index]
+    return {"msg": f"Todo with ID {todo_id} deleted successfully"}
+    return {"msg": "Todo with supplied ID doesn't exist"}
